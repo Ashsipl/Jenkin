f24ef25f4ec4bd3c4ad42cc24c76eb5d60332a52
Ashsipl <ashwini@sipl.co.in>
2024-03-07 10:13:33 +0530

    jenkins

---
+++ GreetingApp.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GreetingApp {
+   public static void main(String[] args) {
+   Scanner scanner = new Scanner(System.in);
+   
+   System.out.print("Enter your name:");
+   String name = scanner.nextLine();
+   
+   scanner.close();
+   System.out.println("Hello, "+ name +"! My baby you are!");
+   }
+   }
+   
+   
+ 
+ 
